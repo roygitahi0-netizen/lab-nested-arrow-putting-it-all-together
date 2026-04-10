01e54d256eb83582f3cf9815173f4d7145acf570
Roy Gitahi <roygitahi0@gmail.com>
2026-04-10 12:07:17 +0300

    Assignment :Add Arrow function and nested function

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
+//Creating a login tracker function by inputting  the userInfo,username,password.
+function createLoginTracker(userInfo, username, password) {
+  return {
+    username: username, //username
+    password: password, //password  
+    loginAttempts: 0, //number of login attempts
+    lastLoginAttempt: null, //timestamp of the last login attempt
+    isLocked: false, //account lock status
+    userInfo: userInfo, //user info object
 
+
+    //Initializing attempt count.
+    initializeAttempts() {
+      this.loginAttempts = 0;
+      this.isLocked = false;
+    },
+
+    //Incrementing attempt count.
+    incrementAttempts() {
+      this.loginAttempts++;
+      if (this.loginAttempts >= 3) {
+        this.isLocked = true;
+      }
+    },
+
+ //Arrow function to check and counts password.
+    checkPassword: () => {  
+      if (this.isLocked) {
+        console.log('Account is locked. Please try again later.');
+        return false;
+      } 
+    }
+  };
+}
 
 
 module.exports = {
